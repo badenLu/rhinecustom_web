6057a298314b4fb6fdac629766961fc91d1be382
Ziang <luziang844@gmail.com>
2025-12-24 19:32:27 +0800

    Added tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,html}", // 确保涵盖了你所有的源代码文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
