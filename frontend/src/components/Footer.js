5b0526c90edae61ea29da789383ec7d89c4d03e9
Ziang <luziang844@gmail.com>
2026-01-16 20:00:28 +0800

    solved the sliding problems of carousel and the alignment of footer elements

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,40 +1,60 @@
 import React from "react";
-import { useTranslation } from "react-i18next"; 
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import "./Footer.css";
-import logo from "../assets/images/logo/logo.png"
+import logo from "../assets/images/logo/logo.png";
+import {SiXiaohongshu, SiTiktok, SiWechat} from 'react-icons/si'
 
 const Footer = () => {
   const { t } = useTranslation();
-  
-  return(
-    <div className="footer-wrapper">
-      <div className="footer-container">
-        <div>
-          <h5>{t("footer.quickLinks")}</h5>
-          <p><Link to="/">{t("footer.link1")}</Link></p>
-          <p><Link to="/routes">{t("footer.link2")}</Link></p>
-          <p><Link to="/contact">{t("footer.link3")}</Link></p>
+
+  return (
+      <footer className="footer-wrapper">
+        <div className="footer-container">
+
+          {/* 上半部分：三列信息 */}
+          <div className="footer-columns">
+
+            <div className="footer-col">
+              <h5 className="footer-title">{t("footer.quickLinks")}</h5>
+              <Link to="/" className="footer-link">{t("footer.link1")}</Link>
+              <Link to="/routes" className="footer-link">{t("footer.link2")}</Link>
+              <Link to="/contact" className="footer-link">{t("footer.link3")}</Link>
+            </div>
+
+            <div className="footer-col">
+              <h5 className="footer-title">{t("footer.contactUs")}</h5>
+              <p>{t("footer.email")}: contact@rhinecustom.com</p>
+              <p>{t("footer.address")}: Merzhauser Str.164,<br />79100 Freiburg, Germany</p>
+              {/* <p>{t("footer.phone")}: +123 456 7890</p> */}
+            </div>
+
+            <div className="footer-col">
+              <h5 className="footer-title">{t("footer.socialMedia")}</h5>
+              <p>
+                <SiXiaohongshu className="social-icon" size={30}/>
+                {t("footer.rednote")}
+              </p>
+              <p>
+                <SiTiktok className="social-icon" />
+                {t("footer.tiktok")}
+              </p>
+              <p>
+                <SiWechat className="social-icon" />
+                {t("footer.wechat")}
+              </p>
+            </div>
+
+            {/* 下半部分：Logo + Legal */}
+            <div className="footer-col footer-legal">
+              <img src={logo} alt="Logo" className="footer-logo" />
+              <p className="footer-subtitle">{t("footer.legalTitle")}</p>
+              <p className="footer-subtitle">{t("footer.copyright")}</p>
+            </div>
+          </div>
+
         </div>
-        <div>
-        <h5>{t("footer.contactUs")}</h5>
-          <p>{t("footer.email")}: contact@rhinecustom.com</p>
-          <p>{t("footer.address")}: Merzhauser Str.164,<br/> 79100 Freiburg, Germany</p>
-          <p>{t("footer.phone")}: +123 456 7890</p>
-        </div>
-        <div>
-        <h5>{t("footer.socialMedia")}</h5>
-          <p>{t("footer.RedNote")}</p>
-          <p>{t("footer.TikTok")}</p>
-          <p>{t("footer.Wechat-Number")}</p>
-        </div>
-        <div>
-          <img src={logo} alt="Logo" className="footer-logo" />
-          <h5>{t("footer.legalTitle")}</h5>
-          <p>{t("footer.copyright")}</p>
-        </div>
-      </div>
-    </div>
+      </footer>
   );
 };
 
